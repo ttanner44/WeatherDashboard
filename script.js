68aa033b9dd3dbf54f8d5b8054643bc2b6df17bc
Tim Tanner <timtanner@tanner-companies.com>
2020-05-08 22:08:44 -0500

    Updated HTML, JS, CSS

---
+++ script.js
@@ -0,0 +1,36 @@
+var apiKey = "630e27fa306f06f51bd9ecbb54aae081";
+var cityName = "Minneapolis";
+var cityState = "Minnesota";
+var zipCode = "55419"
+
+var currentURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var apiIdURL = "&appid="
+var openCurrWeatherAPI = currentURL + cityName + apiIdURL + apiKey;
+
+console.log(openCurrWeatherAPI);
+
+$.ajax({
+url: openCurrWeatherAPI,
+method: "GET"
+}).then(function(response1) {
+
+console.log(response1);
+
+});
+
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var cntURL = "&cnt="
+var apiIdURL = "&appid="
+var numberDays = 5
+var openForecastWeatherAPI = forecastURL + cityName + cntURL + numberDays + apiIdURL + apiKey;
+
+console.log(openForecastWeatherAPI);
+
+$.ajax({
+url: openForecastWeatherAPI,
+method: "GET"
+}).then(function(response2) {
+
+console.log(response2);
+
+});
